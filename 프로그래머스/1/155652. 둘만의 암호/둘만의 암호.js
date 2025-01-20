d67924b019a540aadada40e2187e4743162a501a
Storyos <storyoser@pukyong.ac.kr>
2025-01-20 14:25:09 +0900

    [level 1] Title: 둘만의 암호, Time: 3.92 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
@@ -0,0 +1,40 @@
+function getCharCode(str){
+    var tmp =[];
+    for(let i=0; i<str.length; i++){
+        tmp.push(str.charCodeAt(i));
+    }
+    return tmp;
+}
+
+function addIndex(s,skip,index){
+    for(let i=0; i<s.length; i++){
+        var tmp_index= index;
+        for(let j=0; j<tmp_index; j++){
+            s[i]++;
+            if(s[i]>122){
+                s[i]-=26;
+            }
+            for(let k = 0; k<skip.length;k++){
+                if(s[i]==skip[k]){
+                    tmp_index++;
+                }
+            }
+        }
+    }
+    return s;
+}
+
+function solution(s, skip, index) {
+    var answer = '';
+    var sCharCode = getCharCode(s);
+    var skipCharCode = getCharCode(skip);
+    
+    // console.log(sCharCode);
+    // console.log(skipCharCode);
+    var added = addIndex(sCharCode,skipCharCode,index);
+    console.log(added);
+    for(let i=0; i<added.length; i++){
+        answer+=String.fromCharCode(added[i]);
+    }
+    return answer;
+}
