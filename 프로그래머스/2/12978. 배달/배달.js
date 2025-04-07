6feab099ba9899e712a4337ce579014ace430368
Storyos <storyoser@pukyong.ac.kr>
2025-04-07 15:31:07 +0900

    [level 2] Title: 배달, Time: 6.51 ms, Memory: 35.3 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.js
+++ 프로그래머스/2/12978. 배달/배달.js
@@ -1,29 +1,77 @@
-// 다익스트라 알고리즘
-function solution(N, road, K) {
-    var answer = 0;
-    var tmp = new Set();
-    var roadMap = Array.from(Array(N+1),()=>new Array(N+1).fill(10000000000));
-    for(var [from,to,c] of road){
-        roadMap[from][to]=Math.min(roadMap[from][to],c);
-        roadMap[to][from]=Math.min(roadMap[to][from],c);
+class MinHeap {
+    constructor() {
+        this.heap = [];
     }
-    roadMap[1][1]=0;
-    var queue = [];
-    queue.push([1,0]);
-    while(queue.length>=1){
-        var [node,cost] = queue.pop();
-        for(let i=1; i<=N; i++){
-            if(node!=i&&cost+roadMap[node][i]<=K&&roadMap[1][i]>=roadMap[node][i]+cost){
-                roadMap[1][i]=Math.min(roadMap[node][i]+cost,roadMap[1][i]);
-                roadMap[i][1]=Math.min(roadMap[node][i]+cost,roadMap[i][1]);
-                queue.push([i,cost+roadMap[node][i]]);
+    size() {
+        return this.heap.length;
+    }
+    swap(i, j) {
+        [this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]];
+    }
+    add(value) {
+        this.heap.push(value);
+        this.bubbleUp();
+    }
+    bubbleUp() {
+        let index = this.heap.length - 1;
+        while (index > 0) {
+            let parentIdx = Math.floor((index - 1) / 2);
+            if (this.heap[index][1] >= this.heap[parentIdx][1]) break;
+            this.swap(index, parentIdx);
+            index = parentIdx;
+        }
+    }
+    poll() {
+        if (this.heap.length === 1) return this.heap.pop();
+        const top = this.heap[0];
+        this.heap[0] = this.heap.pop();
+        this.bubbleDown();
+        return top;
+    }
+    bubbleDown() {
+        let index = 0;
+        const length = this.heap.length;
+        while (true) {
+            let left = index * 2 + 1;
+            let right = index * 2 + 2;
+            let smallest = index;
+
+            if (left < length && this.heap[left][1] < this.heap[smallest][1]) smallest = left;
+            if (right < length && this.heap[right][1] < this.heap[smallest][1]) smallest = right;
+            if (smallest === index) break;
+
+            this.swap(index, smallest);
+            index = smallest;
+        }
+    }
+}
+
+function solution(N, road, K) {
+    const heap = new MinHeap();
+    const dist = new Array(N + 1).fill(Infinity);
+    dist[1] = 0;
+
+    const graph = Array.from({ length: N + 1 }, () => []);
+    for (const [from, to, len] of road) {
+        graph[from].push([to, len]);
+        graph[to].push([from, len]);
+    }
+
+    heap.add([1, 0]);
+
+    while (heap.size() > 0) {
+        const [node, cost] = heap.poll();
+
+        if (cost > dist[node]) continue;
+
+        for (const [next, weight] of graph[node]) {
+            const newCost = cost + weight;
+            if (newCost < dist[next]) {
+                dist[next] = newCost;
+                heap.add([next, newCost]);
             }
         }
     }
-    for(let i=1; i<=N; i++){
-        if(roadMap[i][1]<=K){
-         tmp.add(i);   
-        }
-    }
-    return tmp.size;
+
+    return dist.filter(d => d <= K).length;
 }
