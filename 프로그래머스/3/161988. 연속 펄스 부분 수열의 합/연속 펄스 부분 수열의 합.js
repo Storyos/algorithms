1844772089f157912bd71b82f2af844e863b22d8
Storyos <storyoser@pukyong.ac.kr>
2025-04-16 14:20:34 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 225.84 ms, Memory: 67.1 MB -BaekjoonHub

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.js
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.js
@@ -2,14 +2,13 @@
 // slidingwindow?
 function solution(sequence) {
     var dp = [];
-    dp.push([sequence[0],0]);
+    dp.push(sequence[0]);
     for(var i=1; i<sequence.length; i++){
         var sign = i%2===0?1:-1;
-        dp[i]=[dp[i-1][0]+(sequence[i]*sign),i];
+        dp[i]=dp[i-1]+(sequence[i]*sign);
     }
-    dp.sort((a,b)=>a[0]-b[0]);
-    // console.log(dp);
-    var answer = Math.max(Math.abs(dp[0][0]-dp[dp.length-1][0]),Math.abs(dp[0][0]),Math.abs(dp[dp.length-1][0]));
+    dp.sort((a,b)=>a-b);
+    var answer = Math.max(Math.abs(dp[0]-dp[dp.length-1]),Math.abs(dp[0]),Math.abs(dp[dp.length-1]));
     
     return answer;
 }
