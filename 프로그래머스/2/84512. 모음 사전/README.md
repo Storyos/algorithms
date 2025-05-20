3ec080c46d30763b5a9dcad74132a59fe0950314
Storyos <storyoser@pukyong.ac.kr>
2025-05-20 14:18:39 +0900

    [level 2] Title: 모음 사전, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/README.md
+++ 프로그래머스/2/84512. 모음 사전/README.md
@@ -1,10 +1,10 @@
 # [level 2] 모음 사전 - 84512 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512) 
 
 ### 성능 요약
 
-메모리: 38.9 MB, 시간: 33.99 ms
+메모리: 33.4 MB, 시간: 0.09 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 22일 22:29:36
+2025년 05월 20일 14:18:37
 
 ### 문제 설명
 
--- 프로그래머스/2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.js
@@ -1,29 +1,14 @@
-function makeDict(){
-    const word = ['','A','E','I','O','U'];
-    var dict = new Set(); 
-    for(let a = 0; a< 6; a++){
-        for(let b = 0; b<6; b++){
-            for(let c =0; c<6; c++){
-                for(let d=0; d<6; d++){
-                    for(let e=1;e<6; e++){
-                        var tmp = word[e]+word[d]+word[c]+word[b]+word[a];
-                                dict.add(tmp);
-                    }
-                }
-            }
-        }
-    }
-    return dict;
-}
 function solution(word) {
-    var answer = 0;
-    var dict = makeDict();
-    var tmp = [...dict];
-    tmp.sort((a,b)=>a.toString().localeCompare(b.toString()));
-    for(let i=0; i<tmp.length; i++){
-        if(word==tmp[i]){
-            return i+1;
+    //맨앞자리부터 *5+1 
+    let base ="AEIOU"
+    let answer = word.length;
+    let count = 1;
+    for(let i=4; i>=0; i--){
+        if(word[i]&&word[i]!="A"){
+            let diff = base.indexOf(word[i]);
+            answer+=diff*count;
         }
+        count=count*5+1;
     }
     return answer;
 }
