2b4fdb060231bb132f8acf19bc9c899e528b4bb6
Storyos <storyoser@pukyong.ac.kr>
2025-05-23 14:24:36 +0900

    [level 3] Title: 금과 은 운반하기, Time: 137.55 ms, Memory: 47.7 MB -BaekjoonHub

--- 프로그래머스/3/86053. 금과 은 운반하기/금과 은 운반하기.js
+++ 프로그래머스/3/86053. 금과 은 운반하기/금과 은 운반하기.js
@@ -1,33 +1,33 @@
-//필요한 금이랑 은 무게 -> a,b
-//g,s : 각 배열에 있는 금 개수
-
+//가장 빠른 시간 구하기 -> 시간을 중심으로..
 function solution(a, b, g, s, w, t) {
-    const size = g.length;
-    let answer = -1;
+    let size = g.length;
+    var answer = -1;
     let start = 0;
     let end = -1;
-    const arr = [];
-    for(var i=0; i<g.length; i++){
+    for(let i=0; i<size; i++){
+        //(전체 무게 - 처음한번 들고간거)--> 왕복으로 왔다간 횟수 + 제일 처음에 편도
         const v = Math.ceil((g[i]+s[i]-w[i])/w[i])*t[i]*2+t[i];
-        end = Math.max(v,end);
+        end = Math.max(end,v);
     }
     while(start<=end){
         let time = Math.floor((start+end)/2);
+        let totalSilver =0;
         let totalGold = 0;
-        let totalSilver = 0;
-        let total = 0;
+        let totalSum = 0;
         for(let i=0; i<size; i++){
-            let 왕복 = t[i] *2;
-            let moveCount = Math.floor(time/왕복)
-            //첫 편도생각했을때 한번더 가능한거면
-            if(time%왕복>=t[i]) moveCount++;
-            let moveWeight = moveCount * w[i];
-            
-            totalGold+=Math.min(g[i],moveWeight);
-            totalSilver+=Math.min(s[i],moveWeight)
-            total+=Math.min(g[i]+s[i],moveWeight);
+            let comengo = t[i]*2;
+            let movingCount = Math.floor(time/comengo);
+            if(time%comengo>=t[i]) movingCount++;
+            let weightLimit = movingCount*w[i];
+//             w[i] : 한번에 옮길수있는 무게
+            let silver = Math.min(s[i],weightLimit);
+            let gold = Math.min(g[i],weightLimit);
+            let total = Math.min(s[i]+g[i],weightLimit);   
+            totalSilver+=silver;
+            totalGold+=gold;
+            totalSum+=total;
         }
-        if(totalGold>=a&&totalSilver>=b&&total>=a+b){
+        if(totalGold>=a&&totalSilver>=b&&totalSum>=a+b){
             answer=time;
             end=time-1;
         }else{
