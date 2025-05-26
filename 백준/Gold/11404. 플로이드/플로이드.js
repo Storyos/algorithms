02d8c22bb800fc05a28a33a5e8623800cff8e4a5
Storyos <storyoser@pukyong.ac.kr>
2025-05-26 16:47:08 +0900

    [Gold IV] Title: 플로이드, Time: 328 ms, Memory: 21460 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.js
+++ 백준/Gold/11404. 플로이드/플로이드.js
@@ -4,40 +4,43 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 let lc = 0;
-let n, m;
-let busInfo = [];
+let N;
+let M;
+let busInfo;
 rl.on("line", (input) => {
   if (lc === 0) {
-    n = Number(input);
-    busInfo = Array.from(Array(n + 1), () => new Array(n + 1).fill(Infinity));
-    for (let i = 0; i <= n; i++) busInfo[i][i] = 0;
+    N = Number(input);
+    busInfo = Array.from(Array(N + 1), () => new Array(N + 1).fill(Infinity));
   } else if (lc === 1) {
-    m = Number(input);
+    M = Number(input);
   } else {
     let [from, to, cost] = input.split(" ").map(Number);
-    busInfo[from][to] = Math.min(cost, busInfo[from][to]);
-    // busInfo[to][from] = Math.min(cost, busInfo[to][from]);
-    if (lc === m + 1) rl.close();
+    busInfo[from][to] = Math.min(busInfo[from][to], cost);
+    if (lc === M + 1) rl.close();
   }
   lc++;
 });
 
 rl.on("close", () => {
-  //i에서 j = i에서 k + k에서 j
-  for (let i = 1; i <= n; i++) {
-    for (let j = 1; j <= n; j++) {
-      for (let k = 1; k <= n; k++) {
-        busInfo[j][k] = Math.min(busInfo[j][i] + busInfo[i][k], busInfo[j][k]);
+  for (let i = 0; i <= N; i++) busInfo[i][i] = 0;
+
+  for (let k = 1; k <= N; k++) {
+    for (let i = 1; i <= N; i++) {
+      for (let j = 1; j <= N; j++) {
+        busInfo[i][j] = Math.min(busInfo[i][j], busInfo[i][k] + busInfo[k][j]);
       }
     }
   }
-
-  for (let i = 1; i <= n; i++) {
-    let tmp = [];
-    for (let j = 1; j <= n; j++) {
-      busInfo[i][j] = isFinite(busInfo[i][j]) ? busInfo[i][j] : 0;
-      tmp.push(busInfo[i][j]);
+  let answer;
+  for (let i = 1; i < busInfo.length; i++) {
+    let line = [];
+    for (let j = 1; j < busInfo.length; j++) {
+      if (isFinite(busInfo[i][j])) {
+        line.push(busInfo[i][j]);
+      } else {
+        line.push(0);
+      }
     }
-    console.log(tmp.join(" "));
+    console.log(line.join(" "));
   }
 });
