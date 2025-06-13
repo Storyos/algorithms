926bbf1697930812927a7c05cb9bad9fda5aaa46
Storyos <storyoser@pukyong.ac.kr>
2025-06-13 21:25:07 +0900

    [level 5] Title: 상품을 구매한 회원 비율 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
+++ 프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
@@ -1,24 +1,9 @@
-WITH joined_2021 AS (
-    SELECT user_id
-    FROM   user_info
-    WHERE  joined >= DATE '2021-01-01'
-           AND joined <= DATE '2021-12-31'
-    )
- ,
-sale_joined_2021 AS (
-    SELECT     EXTRACT(YEAR FROM s.sales_date) AS year
-             , EXTRACT(MONTH FROM s.sales_date) AS month
-             , COUNT(DISTINCT s.user_id) AS purchased_users
-    FROM       online_sale s
-    INNER JOIN joined_2021 j
-               ON s.user_id = j.user_id
-    GROUP BY   EXTRACT(YEAR FROM s.sales_date)
-             , EXTRACT(MONTH FROM s.sales_date)
-)
-SELECT   year
-       , month
-       , purchased_users
-       , ROUND(purchased_users / (SELECT COUNT(*) FROM joined_2021), 1) AS puchased_ratio
-FROM     sale_joined_2021
-ORDER BY year ASC
-       , month ASC;
+SELECT 
+EXTRACT(YEAR FROM A.SALES_DATE) AS YEAR
+, EXTRACT(MONTH FROM A.SALES_DATE) AS MONTH
+, COUNT(DISTINCT A.USER_ID) AS PURCHASED_USERS
+, ROUND(COUNT(DISTINCT A.USER_ID)/(SELECT COUNT(USER_ID) FROM USER_INFO  WHERE EXTRACT(YEAR FROM JOINED) = '2021'),1) AS PUCHASED_RATIO
+FROM ONLINE_SALE A JOIN USER_INFO B ON B.USER_ID = A.USER_ID
+WHERE EXTRACT(YEAR FROM B.JOINED) = '2021'
+GROUP BY EXTRACT(MONTH FROM A.SALES_DATE),EXTRACT(YEAR FROM A.SALES_DATE)
+ORDER BY YEAR,MONTH 
