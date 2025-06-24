2226ea1d08d4e82ce2400a71bd5a9271c0b44f9b
Storyos <storyoser@pukyong.ac.kr>
2025-06-24 13:56:03 +0900

    [level 2] Title: 귤 고르기, Time: 48.96 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/README.md
+++ 프로그래머스/2/138476. 귤 고르기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 귤 고르기 - 138476 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138476?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138476) 
 
 ### 성능 요약
 
-메모리: 90.8 MB, 시간: 62.15 ms
+메모리: 106 MB, 시간: 48.96 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 14일 23:07:05
+2025년 06월 24일 13:56:02
 
 ### 문제 설명
 
--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -1,23 +1,18 @@
 import java.util.*;
 class Solution {
     public int solution(int k, int[] tangerine) {
-        int answer = 0;
-        HashMap<Integer,Integer> tMap = new HashMap<Integer,Integer>();
-        
-        for(int t:tangerine){
+        int answer =0;
+        HashMap<Integer,Integer> tMap = new HashMap<>();
+        for(Integer t : tangerine){
             tMap.put(t,tMap.getOrDefault(t,0)+1);
         }
-        
-        List<Integer> arr = new ArrayList<>(tMap.values());
-        arr.sort(Collections.reverseOrder());
-        int sum = 0;
-        for(int a : arr){
-            sum+=a;
+        List<Integer> vSet = new ArrayList<>(tMap.values());
+        Collections.sort(vSet,(a,b)->b-a);
+        for(Integer tan : vSet){
+            k-=tan;
             answer++;
-            if(sum>=k) break;
+            if(k<=0) break;
         }
-        
-        
         return answer;
     }
 }
