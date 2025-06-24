8ebc02963f25638b73b096d1abdefc2c5bccf2e6
Storyos <storyoser@pukyong.ac.kr>
2025-06-24 14:31:07 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 205.38 ms, Memory: 133 MB -BaekjoonHub

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/README.md
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/README.md
@@ -1,10 +1,10 @@
 # [level 3] 연속 펄스 부분 수열의 합 - 161988 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/161988?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/161988) 
 
 ### 성능 요약
 
-메모리: 133 MB, 시간: 149.03 ms
+메모리: 133 MB, 시간: 205.38 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 19일 19:21:40
+2025년 06월 24일 14:31:06
 
 ### 문제 설명
 
--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.java
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.java
@@ -1,16 +1,16 @@
 import java.util.*;
 class Solution {
     public long solution(int[] sequence) {
-        long[] dp = new long[sequence.length];
-        dp[0]=sequence[0];
-        for(int i=1; i<sequence.length; i++){
-            int sign = i%2==1?-1:1;
-            dp[i]=dp[i-1]+(sequence[i]*sign);
+        final int len = sequence.length; 
+        long[] acc1 = new long[len];
+        acc1[0] = sequence[0];
+        for(int i=1; i<len; i++){
+            acc1[i]=i%2==0?acc1[i-1]+sequence[i]:acc1[i-1]+sequence[i]*(-1);
         }
-        Arrays.sort(dp);
-        long bigger = Math.max(Math.abs(dp[0]),Math.abs(dp[dp.length-1]));
-        bigger = Math.max(bigger,Math.abs(dp[dp.length-1]-dp[0]));
-        
-        return bigger;
+        Arrays.sort(acc1);
+        long answer = 0;
+        answer = Math.max(Math.abs(acc1[0]),Math.abs(acc1[len-1]));
+        answer = Math.max(answer,Math.abs(acc1[0]-acc1[len-1]));
+        return answer;
     }
 }
