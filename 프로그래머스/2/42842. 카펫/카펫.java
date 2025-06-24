bea176adf03ba5aac65334e16f65d5609272ea66
Storyos <storyoser@pukyong.ac.kr>
2025-06-24 13:21:28 +0900

    [level 2] Title: 카펫, Time: 0.06 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,13 +1,14 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
+        
         int[] answer = new int[2];
-        int xy = brown+yellow;
-        for(int i=3; i<=xy/i; i++){
-            int sero = xy/i;
-            int garo = i;
-            if(xy%i==0&&sero>=3 && garo>=3 && (sero-2)*(garo-2)==yellow){
-                answer[0]=sero;
-                answer[1]=garo;
+        // xy = brown + yellow
+        // 2x+2y = brown + 4
+        for(int x = 1; x<=brown+yellow; x++){
+            int valid = (brown+yellow)%x;
+            if(valid==0&&((brown+yellow)/x+x)*2==brown+4){
+               answer[1]=x;
+               answer[0]=(brown+yellow)/x;
                 break;
             }
         }
