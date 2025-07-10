29b81965b5e4939ac2dac1781f023356fce9aea9
Storyos <storyoser@pukyong.ac.kr>
2025-07-10 19:15:23 +0900

    [level 2] Title: 숫자의 표현, Time: 2.36 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,18 +1,18 @@
 import java.util.*;
+import java.io.*;
 class Solution {
     public int solution(int n) {
-        
         int answer = 0;
+        
+        //자연수 개수
         for(int i=1; i<=n; i++){
-            int tmp = 0;
-           for(int j=i; j<=n; j++){
-               tmp+=j;
-               if(tmp==n){
-                   answer ++;
-                   break; 
-               }
-               else if(tmp>n) break;
-           }
+            for(int start = 1; start<=n; start++){
+                int k = (start*2+i-1)*i/2;
+                if(k>n) break;
+                else if(k==n){
+                answer++;
+                }
+            }
         }
         
         return answer;
